f12842d50626f4358ccc01f98527645f8426a6b6
rajuraj8881 <rajumondalr82@gmail.com>
2019-05-08 02:31:25 +0600

    fixt forwording

--- index.php
+++ index.php
@@ -4,37 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sum Of series</title>
+    <title>PHP Loop</title>
 </head>
 <body>
     <!-- 
     //TODO 
     
-    Number=5 :
-                sum = 1 + 2 + 3 + 4 + 5;
+    Number = 5:
+        sum = 1 + 2 + 3 + 4 + 5;
 -->
 
  <!-- 
     //TODO 
     
-    Number=5 :
-                sub = 5-4-3-2-1;
+    Number = 5:
+        sub = 5 - 4 - 3 - 2 - 1;
 -->
 
  <!-- 
     //TODO 
     
-    Number=5 :
-            5 * 1 = 5
-            5 * 2 = 10
-            5*3=15
-            5*4=20
-            5*5=25
-            5*6=30
-            5*7=35
-            5*8=40
-            5*9=45
-            5*10=50
+    Number = 5:
+        5 * 1 = 5
+        5 * 2 = 10
+        5 * 3 = 15
+        5 * 4 = 20
+        5 * 5 = 25
+        5 * 6 = 30
+        5 * 7 = 35
+        5 * 8 = 40
+        5 * 9 = 45
+        5 * 10 = 50
 
 -->
 
--- summation.php
+++ summation.php
@@ -1,14 +1,16 @@
 <?php
-        $number = $_GET['n'];
-        $sum = 0;
 
-        // for($i = 1; $i <= $number; $i++){
-        //     $sum = $sum + $i;
-        //     echo $i." + ";
-        // }
+// Number = 5:
+//      sum = 1 + 2 + 3 + 4 + 5;
 
-        // echo " = ".$sum.'<br>';
-        
 
-        
-    ?>
+    $number = $_GET['n'];
+    $sum = 0;
+
+    for($i = 1; $i <= $number; $i++){
+        $sum = $sum + $i;
+        echo $i." + ";
+    }
+
+    echo " = ".$sum.'<br>';
+?>
