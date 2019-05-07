f4790aa88b80399df89350ab05848c4a9df93092
rajuraj8881 <rajumondalr82@gmail.com>
2019-05-08 02:19:06 +0600

    Multiplication table

--- index.php
+++ index.php
@@ -21,7 +21,24 @@
                 sub = 5-4-3-2-1;
 -->
 
-    <form  action="subtraction.php" method="get">
+ <!-- 
+    //TODO 
+    
+    Number=5 :
+            5 * 1 = 5
+            5 * 2 = 10
+            5*3=15
+            5*4=20
+            5*5=25
+            5*6=30
+            5*7=35
+            5*8=40
+            5*9=45
+            5*10=50
+
+-->
+
+    <form  action="multiplication-table.php" method="get">
         <input type="number" name="n"/>
         <input type="submit" value="calculate" />
     </form>
